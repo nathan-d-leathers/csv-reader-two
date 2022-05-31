a1b4f51c20aa64071fab18d6b9ba349636d67e12
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-31 13:41:54 -0700

    solved in py

---
+++ main.py
@@ -0,0 +1,12 @@
+animal = input("insert an animal: ")
+if animal[-1] != "s":
+    animal = animal + "s"
+try:
+    with open(f"data/{animal}.csv") as animals:
+        for x in animals.readlines()[1:]:
+            line = x.strip().split(", ")
+            print(f"{line[0].capitalize()} is a {line[1]} year old {line[2].title()} available for adoption.")
+
+
+except Exception:
+    print(f"{animal} are not available for adoption")
